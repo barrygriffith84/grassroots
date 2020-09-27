b79c1b8ede9ff0caa876debb7d5175045e3fdf7d
Barry Griffith <barrygriffith@outlook.com>
2020-09-27 17:18:13 -0400

    redirect for register is complete.  log in needs to be tested.

--- grassroots/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ grassroots/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -100,15 +100,10 @@ namespace grassroots.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+
+                 
                 }
                 foreach (var error in result.Errors)
                 {
