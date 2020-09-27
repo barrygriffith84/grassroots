4e3419939f9029a4c612b2184888cf1347579d51
Barry Griffith <barrygriffith@outlook.com>
2020-09-26 22:50:41 -0400

    Basic register is done.  Need to change redirect pages.

---
+++ grassroots/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using grassroots.Data;
+using grassroots.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(grassroots.Areas.Identity.IdentityHostingStartup))]
+namespace grassroots.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
